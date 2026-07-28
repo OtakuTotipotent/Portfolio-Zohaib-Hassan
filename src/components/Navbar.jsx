6a290e7d011379e7c0e59f757f863c82c0ecddff
OtakuTotipotent <otakutotipotent@gmail.com>
2026-07-28 12:48:39 +0500

    feat: add navbar; add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
+
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [Active, setActive] = useState("");
 
-export default Navbar
+  return (
+    <nav
+      className={`
+      ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary
+      `}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Zohaib <span className="sm:block hidden">Hassan</span>
+          </p>
+        </Link>
+        <p className="text-red-500">Menu</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
